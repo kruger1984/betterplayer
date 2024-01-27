03b54ead57cc8360581f3743ca41fbe52019bbbe
Haris Rubanović <haris@stagecoding.com>
2024-01-27 11:21:54 +0100

    duration fix

--- ios/Classes/BetterPlayer.m
+++ ios/Classes/BetterPlayer.m
@@ -439,7 +439,8 @@ static inline CGFloat radiansToDegrees(CGFloat radians) {
         if (!_player.currentItem) {
             return;
         }
-        if (_player.status != AVPlayerStatusReadyToPlay) {
+        if (_player.currentItem.status != AVPlayerItemStatusReadyToPlay 
+            || _player.status != AVPlayerStatusReadyToPlay) {
             return;
         }
 
